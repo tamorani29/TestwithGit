3d3b4761fe97f007e256444b4db262ff4e6f2029
Ash Gup <ashishtestdevops@outlook.com>
2021-09-01 22:11:17 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestwithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
